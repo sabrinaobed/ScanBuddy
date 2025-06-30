ffc5b312b7708421e999cc186d84b346f5b0f39e
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-06-30 11:24:07 +0200

    Added properties to UserLoginDTO

--- ClassLibrary.ScanBuddy/Backend.DTOs/UserLoginDTO.cs
+++ ClassLibrary.ScanBuddy/Backend.DTOs/UserLoginDTO.cs
@@ -8,5 +8,7 @@ namespace ClassLibrary.ScanBuddy.Backend.DTOs
 {
     public class UserLoginDTO
     {
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
