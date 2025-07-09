8c3dcd1dd8ad170ad87d1f292b93a000d04e91d4
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-07-09 21:26:26 +0200

    Set methods in IAuthService in Backend.Interfaces

---
+++ ClassLibrary.ScanBuddy/Backend.Interfaces/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using ClassLibrary.ScanBuddy.Backend.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.ScanBuddy.Backend.Interfaces
+{
+    internal interface IAuthService
+    {
+        Task<string> RegisterAsync(UserRegistrationDTO dto);
+        Task<string>LoginAsync(UserLoginDTO dto);
+    }
+}
