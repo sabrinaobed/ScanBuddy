7ae1648e535fff66cba47e680d8475650ff16609
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-07-11 12:53:52 +0200

    Registered JWT settings in program.cs

--- ScanBuddy/Program.cs
+++ ScanBuddy/Program.cs
@@ -1,4 +1,7 @@
-
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using ScanBuddy.JWTConfiguration;
+using System.Text;
 using Microsoft.EntityFrameworkCore;
 using ScanBuddy.Context;
 
@@ -21,6 +24,34 @@ namespace ScanBuddy
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
+            // Bind JwtSettings from appsettings.json
+            builder.Services.Configure<JwtSettings>(
+                builder.Configuration.GetSection("JwtSettings"));
+
+            var jwtSettings = builder.Configuration
+                .GetSection("JwtSettings").Get<JwtSettings>();
+
+            // Configure JWT Authentication
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings.Issuer,
+                    ValidAudience = jwtSettings.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(jwtSettings.SecretKey))
+                };
+            });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -30,6 +61,7 @@ namespace ScanBuddy
             }
 
             app.UseHttpsRedirection();
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
