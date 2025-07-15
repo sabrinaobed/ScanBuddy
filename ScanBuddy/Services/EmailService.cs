e8e9c7a3837076842aaf9fbd11d4b1470a278cb9
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-07-15 19:05:29 +0200

    Created EmailService and set all logic for smtp to send otp via email

---
+++ ScanBuddy/Services/EmailService.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Net.Mail;
+using ClassLibrary.ScanBuddy.Backend.Interfaces;
+
+namespace ScanBuddy.Services
+{
+    public class EmailService :IEmailService
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string body)
+        {
+            var smtpClient = new SmtpClient
+            {
+                Host = _configuration["Smtp:Host"],
+                Port = int.Parse(_configuration["Smtp:Port"]),
+                EnableSsl = true,
+                Credentials = new NetworkCredential(
+                    _configuration["Smtp:Username"],
+                    _configuration["Smtp:Password"])
+
+            };
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress(_configuration["Smtp:From"]),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = false
+
+            };
+
+
+            mailMessage.To.Add(toEmail);
+
+            await smtpClient.SendMailAsync(mailMessage);
+        }
+    }
+}
