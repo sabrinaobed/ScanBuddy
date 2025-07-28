83b31ce8ca05af200a642f618527f381b7ab4b94
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-07-28 10:39:27 +0200

    Created IAuthService in frontend interfaces folder with all set of methods

---
+++ ClassLibrary.ScanBuddy/Frontend.Interfaces/IAuthService.cs
@@ -0,0 +1,20 @@
+﻿using ClassLibrary.ScanBuddy.Frontend.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.ScanBuddy.Frontend.Interfaces
+{
+    public  interface IAuthService
+    {
+        Task<string> RegisterAsync(RegistrationDTO dto);
+
+        Task<string> VerifyRegistrationOtpAsync(VerifyRegistrationOtpDTO dto);
+
+        Task<string> LoginAsync(LoginDTO dto);
+
+        Task<string> VerifyLoginOtpAsync(VerifyLoginOtpDTO dto);
+    }
+}
