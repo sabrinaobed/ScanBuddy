ad1a2b1f90e4cad9488e1da9668ec8b928ad5bdb
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-10-14 19:38:47 +0200

    made ApplicationDbContext DI-only and remove OnConfiguring

--- ScanBuddy/Context/ApplicationDbContext.cs
+++ ScanBuddy/Context/ApplicationDbContext.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using ScanBuddy.Models;
 
 namespace ScanBuddy.Context
@@ -8,37 +7,35 @@ namespace ScanBuddy.Context
     public class ApplicationDbContext : DbContext 
     {
         //Creates a table in the SQL databse for ApplicationUser model
-        public DbSet<ApplicationUser> Users { get; set; }
+        public DbSet<ApplicationUser> Users { get; set; } = default;
 
-        //At design time, EF Core uses the parameterless constructor, which then calls your OnConfiguring() method to:Load your appsettings.json,Read the connection string,Configure SQL Server
-        public ApplicationDbContext() { }
-
-
+     
 
         //This constructor accepts DbContextOptions which are used to configure the database connection and other options via dependency injection
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options) { } 
+            : base(options) { }
 
+        /*
+                //This method is called by the framework to configure the database context and it is only called if the context is not already configured in program.cs.
+                protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)//(Loads the connection string from appsettings.json and Tells EF to use SQL Server as the database)
+                {
+                    if (!optionsBuilder.IsConfigured)
+                    {
+                        //Loads the configuration settings from appsettings.json
+                        var configuration = new ConfigurationBuilder()
+                            .SetBasePath(Directory.GetCurrentDirectory()) //set the base path to the project directory
+                            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)//load the appsettings.json file, which contains the database connection string and other settings
+                            .Build();
 
-        //This method is called by the framework to configure the database context and it is only called if the context is not already configured in program.cs.
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)//(Loads the connection string from appsettings.json and Tells EF to use SQL Server as the database)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                //Loads the configuration settings from appsettings.json
-                var configuration = new ConfigurationBuilder()
-                    .SetBasePath(Directory.GetCurrentDirectory()) //set the base path to the project directory
-                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)//load the appsettings.json file, which contains the database connection string and other settings
-                    .Build();
+                        //Retreive the connection string named "Default Connection" 
+                        var connectionString = configuration.GetConnectionString("DefaultConnection");
 
-                //Retreive the connection string named "Default Connection" 
-                var connectionString = configuration.GetConnectionString("DefaultConnection");
-
-                //Use the connection string to configure the database context to use SQL Server
-                optionsBuilder.UseSqlServer(connectionString);
-            }
-        }
+                        //Use the connection string to configure the database context to use SQL Server
+                        optionsBuilder.UseSqlServer(connectionString);
+                    }
+                }
+        */
 
         //This method is called by the framework to configure the model when the database is created
         protected override void OnModelCreating(ModelBuilder modelBuilder) //(This lets you define rules like:Which columns are unique,Relationships between tables,Table/column names if you want to customize them)
